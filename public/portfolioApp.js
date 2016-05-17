8caab2cdaee9cff94e815ea9015e7a5cf00bf778
Spencer Powell <spenc84@gmail.com>
2016-05-17 02:16:11 -0500

    Updated some items on the profile

--- public/portfolioApp.js
+++ public/portfolioApp.js
@@ -18,6 +18,26 @@ angular.module('portfolioApp', ['ui.router'])
     templateUrl: '/portfolio/portfolio.html',
     parent: 'home'
   })
+      .state('trax', {
+        url: '/portfolio/trax',
+        templateUrl: '/portfolio/trax.html',
+        parent: 'home'
+      })
+      .state('life', {
+        url: '/portfolio/life',
+        templateUrl: '/portfolio/life.html',
+        parent: 'home'
+      })
+      .state('rockPaperScissors', {
+        url: '/portfolio/rockPaperScissors',
+        templateUrl: '/portfolio/rockPaperScissors.html',
+        parent: 'home'
+      })
+      .state('vidFinder', {
+        url: '/portfolio/vidFinder',
+        templateUrl: '/portfolio/vidFinder.html',
+        parent: 'home'
+      })
   .state('resume', {
     url: '/resume',
     templateUrl: '/resume/resume.html',
